3af98f0b86004c0c0692715b0c8e924181fda04c
yoonwooiOS <yoonwoo4429@kakao.com>
2024-10-11 00:43:30 +0900

    Feat: SearchView UI 구현

---
+++ PocketTheater/Presentation/Search/SearchViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  SearchViewModel.swift
+//  PocketTheater
+//
+//  Created by 김윤우 on 10/10/24.
+//
+
+import Foundation
+
+class SearchViewModel: ViewModelType {
+   
+    struct Input {
+        
+    }
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+    
+    
+    
+}
