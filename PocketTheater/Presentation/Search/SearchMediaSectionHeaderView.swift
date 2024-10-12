0931dc83c6a4a94c41a6b8d0d63028da466df2d8
yoonwooiOS <yoonwoo4429@kakao.com>
2024-10-12 20:41:30 +0900

    WIP: pbx오류 확인중

---
+++ PocketTheater/Presentation/Search/SearchMediaSectionHeaderView.swift
@@ -0,0 +1,33 @@
+//
+//  SearchMediaSectionHeaderView.swift
+//  PocketTheater
+//
+//  Created by 김윤우 on 10/12/24.
+//
+
+import UIKit
+
+class MediaSectionHeaderView: UICollectionReusableView {
+    
+    private let titleLabel = UILabel().then {
+        $0.font = Resource.Font.bold16
+        $0.textColor = Resource.Color.white
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(8)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with title: String) {
+        titleLabel.text = title
+    }
+}
--- PocketTheater/Presentation/Search/SearchView.swift
+++ PocketTheater/Presentation/Search/SearchView.swift
@@ -11,22 +11,20 @@ import Then
 
 class SearchView: BaseView {
     
-    private let searchBar = UISearchBar().then {
+    let searchBar = UISearchBar().then {
         $0.placeholder = "게임, 시리즈, 영화를 검색하세요."
         $0.setImage(UIImage(), for: UISearchBar.Icon.search, state: .normal)
         $0.barStyle = .black
         $0.searchBarStyle = .minimal
     }
     
-    private lazy var searchCollectionView = UICollectionView(frame: .zero, collectionViewLayout: Resource.CollectionViewLayout.MediaLayout()).then {
-           $0.register(MediaCollectionViewCell.self, forCellWithReuseIdentifier: MediaCollectionViewCell.identifier)
-           $0.dataSource = self
-       }
-    private let dummyData = ["image1", "image2", "image3", "image4", "image5", "image6","image1", "image2", "image3", "image4", "image5", "image6","image1", "image2", "image3", "image4", "image5", "image6"]
-    
+    let searchCollectionView = UICollectionView(frame: .zero, collectionViewLayout: Resource.CollectionViewLayout.createMediaPlayCellLayout()).then {
+        $0.register(MediaPlayCollectionViewCell.self, forCellWithReuseIdentifier: MediaPlayCollectionViewCell.identifier)
+        $0.backgroundColor = Resource.Color.black
+    }
     override func setHierarchy() {
-        self.addSubview(searchBar)
-        self.addSubview(searchCollectionView)
+        addSubview(searchBar)
+        addSubview(searchCollectionView)
     }
     
     override func setLayout() {
@@ -34,45 +32,11 @@ class SearchView: BaseView {
             make.top.horizontalEdges.equalTo(safeAreaLayoutGuide)
         }
         searchCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(searchBar.snp.bottom).offset(8)
+            make.top.equalTo(searchBar.snp.bottom)
             make.horizontalEdges.bottom.equalTo(safeAreaLayoutGuide)
         }
     }
    
-    static func createTableViewLayout() -> UICollectionViewCompositionalLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                              heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
- 
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                               heightDimension: .fractionalWidth(0.33))
- 
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
-
-        let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
-
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
-    
-    }
-    
-}
-
-extension SearchView: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return dummyData.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCollectionViewCell.identifier, for: indexPath) as? MediaCollectionViewCell else {
-                    return UICollectionViewCell()
-                }
-        
-        return cell
-    }
     
     
 }
